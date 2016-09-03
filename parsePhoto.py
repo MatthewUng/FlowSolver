dc108af36217e83247ab3baf383905cfa810de44
Matthew Ung <matthew.ung814@gmail.com>
2016-09-03 02:10:11 -0700

    convert photo to matrix

--- parsePhoto.py
+++ parsePhoto.py
@@ -3,14 +3,26 @@ import os
 
 class ImageParser:
     #(0,0,0) is black
-    #(104, 169, 66)
+    #outline color
+    #(123,123,63)
+    
     def __init__(self, filename):
         self.name = filename
         self.directory = os.path.dirname(os.path.realpath(__file__))
-        self.image = Image.open(self.directory+ '\\' + filename)
+        self.array = self.toArray(Image.open(self.directory+ '\\' + filename))
 
     def parse(self):
         pass
+    
+
+    def toArray(self, image):
+        #size = (w,h)
+        data = list(image.getdata())
+        size = image.size
+        out = list()
+        for i in range(size[1]):
+            out.append(data[size[0]*i:size[0]*(i+1)])
+        return out
 
     def save(self):
         self.image.save(self.directory+r"\test\{}".format(os.path.basename(self.name)))
@@ -24,7 +36,9 @@ class ImageParser:
 
 
 if __name__ == '__main__':
-    l = [ [(104,169,66) for _ in range(100)]for _ in range(100)]    
-
-
+    #(1440, 2560)  width x height
+    directory = os.path.dirname(os.path.realpath(__file__))
+    image = Image.open(directory+'\\'+r'\test\7x7v1.png')
+    size = image.size
+    
 
